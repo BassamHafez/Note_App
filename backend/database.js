6c20588b18b2bc5dd8e934137d06e2a370d2e265
BassamHafez <bassamhafez790@gmail.com>
2024-02-16 18:55:04 +0200

    ,

--- backend/database.js
+++ backend/database.js
@@ -1,8 +1,8 @@
 const mysql = require("mysql2");
 
-const DB_NAME = "";
-const DB_USER = "";
-const DB_PASSWORD = "";
+const DB_NAME = "notes_db";
+const DB_USER = "root";
+const DB_PASSWORD = "root";
 
 const pool = mysql.createPool({
   host: "localhost",
@@ -12,23 +12,22 @@ const pool = mysql.createPool({
 });
 
 pool.query(
-  `CREATE TABLE IF NOT EXISTS users (
+  `CREATE TABLE if not exists  users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
-  )`
-);
+    password VARCHAR(255) NOT NULL ) ;
+   `
+)
 
 pool.query(
-  `CREATE TABLE IF NOT EXISTS notes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    body TEXT NOT NULL,
-    priority ENUM('low', 'med', 'high') NOT NULL,
-    user_id INT,
-    FOREIGN KEY (user_id) REFERENCES users(id)
-  )`
-);
+  ` CREATE TABLE if not exists  notes (
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   title VARCHAR(255) NOT NULL,
+   body TEXT NOT NULL,
+   priority ENUM('low', 'med', 'high') NOT NULL,
+   user_id INT,
+   FOREIGN KEY (user_id) REFERENCES users(id) ) ;`
+ ) ;
 
 module.exports = pool.promise();
--- frontend/src/Components/UI/NoteBox.jsx
+++ frontend/src/Components/UI/NoteBox.jsx
@@ -3,17 +3,38 @@ import Col from "react-bootstrap/Col";
 import styles from "./NoteBox.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faTrashCan } from "@fortawesome/free-regular-svg-icons";
-import { faPencil} from "@fortawesome/free-solid-svg-icons";
+import { faCircle} from "@fortawesome/free-solid-svg-icons";
 
-const NoteBox = ({title,desc}) => {
+const NoteBox = ({title,desc,priority}) => {
+
+let circleColor="low"
+
+switch (priority) {
+  case "low":
+    circleColor=styles.low
+    break;
+  case "medium":
+    circleColor=styles.medium
+    break;
+  case "high":
+    circleColor=styles.high
+    break;
+
+  default:
+    break;
+}
+
   return (
     <Col md={6} lg={4}>
       <div className={styles.note_box}>
+        <div className={styles.priority_container}>
+        <FontAwesomeIcon icon={faCircle} className={circleColor}/>
+        </div>
         <h2>{title}</h2>
         <p>{desc}</p>
-        <div className={`${styles.box_footer} d-flex justify-content-end`}>
-          <FontAwesomeIcon title="update" className={styles.update_icon} icon={faPencil} />
+        <div className={`${styles.box_footer} d-flex justify-content-between w-100 align-items-center px-2`}>
           <FontAwesomeIcon  title="delete"  className={styles.delete_icon} icon={faTrashCan} />
+            <button className={styles.update_btn}>Update</button>
         </div>
       </div>
     </Col>
